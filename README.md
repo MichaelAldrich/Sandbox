88d9136e87424567aef4bf35f181a99d2c7e5813
Path3tic <11843596+MichaelAldrich@users.noreply.github.com>
2019-09-14 01:14:42 -0400

    build status as well as fixed syntax for test file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.com/MichaelAldrich/Sandbox.svg?branch=master)](https://travis-ci.com/MichaelAldrich/Sandbox)
+
 # Sandbox
 It's the sandbox, where things will only be working long enough for me to look at them.
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import time
 
-print('hello from the testing world')
-print('it is ' + str(time.time()))
-if 2 > 1:
-    return 0
-else:
-    return 1
+def test():
+    print('hello from the testing world')
+    print('it is ' + str(time.time()))
+    if 2 > 1:
+        return 0
+    else:
+        return 1
